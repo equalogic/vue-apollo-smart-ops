22787d57e44b7923859c168b4f2a648fb5e569d9
Simon Garner <simon.garner@madscience.co.nz>
2021-09-05 07:55:23 +1200

    Add SmartQuery decorator

---
+++ src/decorator/SmartQuery.ts
@@ -0,0 +1,20 @@
+/*
+ * Based on https://github.com/chanlito/vue-apollo-decorator by chanlito ❤️
+ */
+
+import { ApolloError, OperationVariables } from 'apollo-client';
+import { DocumentNode } from 'graphql';
+import Vue from 'vue';
+import { createDecorator, VueDecorator } from 'vue-class-component';
+import { VueApolloSmartQueryOptions } from '../query';
+
+export function SmartQuery<TApp = any, TResult = any, TVariables = OperationVariables, TError = ApolloError>(
+  options: TApp extends Vue
+    ? VueApolloSmartQueryOptions<TResult, TVariables, TError, TApp>
+    : DocumentNode,
+): VueDecorator {
+  return createDecorator((componentOptions: any, k: string) => {
+    componentOptions.apollo = componentOptions.apollo || {};
+    componentOptions.apollo[k] = options;
+  });
+}
